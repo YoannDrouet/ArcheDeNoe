a12255a40ed0ed47d4e7f09e0fb08cf9ad8b8222
YoannDrouet <yoanndrouet@hotmail.fr>
2021-05-20 09:54:26 +0200

    Ajout classe SaisieUtitlisateur

---
+++ src/arche/bo/SaisieUtitlisateur.java
@@ -0,0 +1,154 @@
+package arche.bo;
+
+import arche.animaux.*;
+import arche.enumeration.Espece;
+import arche.enumeration.Sexe;
+import arche.exception.HorsBornesException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SaisieUtitlisateur {
+    private static Scanner scan = new Scanner(System.in);
+    private static Verifs verif = new Verifs();
+
+    public String NomAnimal(){
+        /// • Demande du nom de l'animal à l'utilisateur • ///
+
+        System.out.println("Quel est le nom de l'animal ?");
+        String reponse_utilisateur_nom_animal = scan.nextLine();
+    }
+
+    public int SexeAnimal(){
+        /// • Demande du sexe de l'animal à l'utilisateur • ///
+
+        int reponse_utilisateur_sexe_animal;
+
+        // Mise en forme de l'énumération Sexe sous forme de tableau
+        Sexe[] tabSexe = Sexe.values();
+
+        // Affichage de la question
+        System.out.printf("Quel est le sexe de l'animal ? ");
+
+        // Affichage des choix possibles
+        for (int i = 0; i < Sexe.values().length; i++) {
+            System.out.printf("%d-%s ",i, tabSexe[i]);
+        }
+        System.out.println();
+
+        do {
+            try {
+                reponse_utilisateur_sexe_animal = scan.nextInt();
+
+                // Vérifie que l'utilisateur a bien saisi une réponse valide
+                verif.intBorne(reponse_utilisateur_sexe_animal, Sexe.values().length - 1);
+
+                // Si oui on sort
+                break;
+            }
+
+            // Exception si l'utilisateur a saisi autre chose qu'un entier
+            catch (InputMismatchException e) {
+                System.err.println("Entrez un des nombres proposés");
+            }
+
+            // Exception si l'utilisateur a saisi un entier non compris dans ceux à sa disposition
+            catch (HorsBornesException e) {
+                System.err.println(e.getMessage());
+            }
+            finally {
+                scan.nextLine();
+            }
+
+            // Boucle tant que l'utilisateur n'a pas saisi une réponse valide
+        } while (true);
+    }
+
+    public int EspeceAnimal(){
+        /// • Demande de l'espèce de l'animal à l'utilisateur • ///
+
+        int reponse_utilisateur_espece_animal;
+
+        // Mise en forme de l'énumération Espece sous forme de tableau
+        Espece[] tabEspece = Espece.values();
+
+        // Affichage de la question
+        System.out.println("Quel est l'espèce de l'animal ?");
+
+        // Affichage des choix possibles
+        for (int i = 0; i < Espece.values().length; i++) {
+            System.out.printf("%d-%s ", i, tabEspece[i]);
+        }
+        System.out.println();
+
+        do {
+            try {
+                reponse_utilisateur_espece_animal = scan.nextInt();
+
+                // Vérifie que l'utilisateur a bien saisi une réponse valide
+                verif.intBorne(reponse_utilisateur_espece_animal, Espece.values().length - 1);
+
+                // Si oui on sort
+                break;
+            }
+            // Exception si l'utilisateur a saisi autre chose qu'un entiers
+            catch (InputMismatchException e) {
+                System.err.println("Entrez un des nombres proposés");
+            }
+
+            // Exception si l'utilisateur a saisi un entier non compris dans ceux à sa disposition
+            catch (HorsBornesException e) {
+                System.err.println(e.getMessage());
+
+            }
+            finally {
+                scan.nextLine();
+            }
+
+            // Boucle tant que l'utilisateur n'a pas saisi une réponse valide
+        } while (true);
+    }
+
+    public Animal AnimalUtilisateur(){
+        String nom = this.NomAnimal();
+
+
+            int sexe = this.SexeAnimal();
+            int espece = this.EspeceAnimal();
+
+        /// • Initialisation de l'animal en fonction des saisies de l'utilisateur • ///
+
+        switch (reponse_utilisateur_espece_animal) {
+
+            // Création d'un Chat
+            case 0:
+                animal_utilisateur = new Chat(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
+                break;
+
+            // Création d'un Chien
+            case 1:
+                animal_utilisateur = new Chien(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
+                break;
+
+            // Création d'un Gorille
+            case 2:
+                animal_utilisateur = new Gorille(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
+                break;
+
+            // Création d'un Lapin
+            case 3:
+                animal_utilisateur = new Lapin(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
+                break;
+        }
+        return animal
+
+
+
+
+
+
+
+    }
+
+    public int saisie
+}
--- src/arche/main/main.java
+++ src/arche/main/main.java
@@ -24,7 +24,7 @@ public class main {
 
         // Déclaration des instances
         Scanner scan = new Scanner(System.in);
-        Verifs verif = new Verifs();
+
 
         // Déclaration des variables
         int reponse_utilisateur_continuer;
@@ -45,127 +45,10 @@ public class main {
 
 
 
-            /// • Demande du nom de l'animal à l'utilisateur • ///
-
-            System.out.println("Quel est le nom de l'animal ?");
-            String reponse_utilisateur_nom_animal = scan.nextLine();
 
 
 
-            /// • Demande du sexe de l'animal à l'utilisateur • ///
 
-            int reponse_utilisateur_sexe_animal;
-
-            // Mise en forme de l'énumération Sexe sous forme de tableau
-            Sexe[] tabSexe = Sexe.values();
-
-            // Affichage de la question
-            System.out.printf("Quel est le sexe de l'animal ? ");
-
-            // Affichage des choix possibles
-            for (int i = 0; i < Sexe.values().length; i++) {
-                System.out.printf("%d-%s ",i, tabSexe[i]);
-            }
-            System.out.println();
-
-            do {
-                try {
-                    reponse_utilisateur_sexe_animal = scan.nextInt();
-
-                    // Vérifie que l'utilisateur a bien saisi une réponse valide
-                    verif.intBorne(reponse_utilisateur_sexe_animal, Sexe.values().length - 1);
-
-                    // Si oui on sort
-                    break;
-                }
-
-                // Exception si l'utilisateur a saisi autre chose qu'un entier
-                catch (InputMismatchException e) {
-                    System.err.println("Entrez un des nombres proposés");
-                }
-
-                // Exception si l'utilisateur a saisi un entier non compris dans ceux à sa disposition
-                catch (HorsBornesException e) {
-                    System.err.println(e.getMessage());
-                }
-                finally {
-                    scan.nextLine();
-                }
-
-            // Boucle tant que l'utilisateur n'a pas saisi une réponse valide
-            } while (true);
-
-
-
-            /// • Demande de l'espèce de l'animal à l'utilisateur • ///
-
-            int reponse_utilisateur_espece_animal;
-
-            // Mise en forme de l'énumération Espece sous forme de tableau
-            Espece[] tabEspece = Espece.values();
-
-            // Affichage de la question
-            System.out.println("Quel est l'espèce de l'animal ?");
-
-            // Affichage des choix possibles
-            for (int i = 0; i < Espece.values().length; i++) {
-                System.out.printf("%d-%s ", i, tabEspece[i]);
-            }
-            System.out.println();
-
-            do {
-                try {
-                    reponse_utilisateur_espece_animal = scan.nextInt();
-
-                    // Vérifie que l'utilisateur a bien saisi une réponse valide
-                    verif.intBorne(reponse_utilisateur_espece_animal, Espece.values().length - 1);
-
-                    // Si oui on sort
-                    break;
-                }
-                // Exception si l'utilisateur a saisi autre chose qu'un entiers
-                catch (InputMismatchException e) {
-                    System.err.println("Entrez un des nombres proposés");
-                }
-
-                // Exception si l'utilisateur a saisi un entier non compris dans ceux à sa disposition
-                catch (HorsBornesException e) {
-                    System.err.println(e.getMessage());
-
-                }
-                finally {
-                    scan.nextLine();
-                }
-
-            // Boucle tant que l'utilisateur n'a pas saisi une réponse valide
-            } while (true);
-
-
-
-            /// • Initialisation de l'animal en fonction des saisies de l'utilisateur • ///
-
-            switch (reponse_utilisateur_espece_animal) {
-
-                // Création d'un Chat
-                case 0:
-                    animal_utilisateur = new Chat(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
-                    break;
-
-                // Création d'un Chien
-                case 1:
-                    animal_utilisateur = new Chien(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
-                    break;
-
-                // Création d'un Gorille
-                case 2:
-                    animal_utilisateur = new Gorille(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
-                    break;
-
-                // Création d'un Lapin
-                case 3:
-                    animal_utilisateur = new Lapin(reponse_utilisateur_nom_animal, tabSexe[reponse_utilisateur_sexe_animal]);
-                    break;
-            }
 
 
 
